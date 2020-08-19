65cd3be5b8501fe0c14e5458ec64a64d7884a74c
helax000 <364033372@qq.com>
2020-08-19 12:39:24 +0800

    .

--- checkpsw.sh
+++ checkpsw.sh
@@ -13,19 +13,19 @@ TIME_STAMP=`date "+%Y-%m-%d %T"`
 
 ###########################################################
 
-if [[ ! -r "${PASSFILE}" ]]; then
+if [ ! -r "${PASSFILE}" ]; then
   echo "${TIME_STAMP}: Could not open password file \"${PASSFILE}\" for reading." >> ${LOG_FILE}
   exit 1
 fi
 
 CORRECT_PASSWORD=`awk '!/^;/&&!/^#/&&$1=="'${username}'"{print $2;exit}' ${PASSFILE}`
 
-if [[ "${CORRECT_PASSWORD}" = "" ]]; then
+if [ "${CORRECT_PASSWORD}" = "" ]; then
   echo "${TIME_STAMP}: User does not exist: username=\"${username}\", password=\"${password}\"." >> ${LOG_FILE}
   exit 1
 fi
 
-if [[ "${password}" = "${CORRECT_PASSWORD}" ]]; then
+if [ "${password}" = "${CORRECT_PASSWORD}" ]; then
   echo "${TIME_STAMP}: Successful authentication: username=\"${username}\"." >> ${LOG_FILE}
   exit 0
 fi
